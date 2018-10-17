6de5b77644f4464483a15c24707b6b7cd72cfe01
Nazmus <nsakib6685@bths.edu>
2018-10-17 00:27:05 -0400

    UPDATE 10/17 12:26 AM

--- src/SakibBotDir/Siblings.java
+++
@@ -1,8 +0,0 @@
-package SakibBotDir;
-
-public class Siblings extends Person {
-    public Siblings(String firstName, String lastName)
-    {
-        super(firstName, lastName,"sibling");
-    }
-}
--- src/SakibBotDir/Teacher.java
+++ src/SakibBotDir/Teacher.java
@@ -2,6 +2,11 @@ package SakibBotDir;
 
 public class Teacher extends Person {
 
+    private String firstName;
+    private String lastName;
+    private String relationshipType = "teacher";
+    private String gender;
+
     public Teacher(String firstName, String lastName)
     {
         super(firstName,lastName,"teacher");
